aae9a0519829465b1b8748c801d76a72df577b92
Nicolas Gonzalez <rocketnick290804@gmail.com>
2026-02-03 23:51:27 -0500

    refactor: was added more test function

--- contracts/test/gameToken.t.sol
+++ contracts/test/gameToken.t.sol
@@ -4,13 +4,17 @@ pragma solidity ^0.8.30;
 import {Test} from "forge-std/Test.sol";
 import {GameToken} from "../src/gameToken.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC20Errors} from "lib/openzeppelin-contracts/contracts/interfaces/draft-IERC6093.sol";
+
 
 contract GameTokenTest is Test {
     GameToken public gameToken;
     address public constant OWNER = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
     uint256 public constant DECIMALS = 18;
     uint256 public constant INITIAL_SUPPLY = 10_000 * 10 ** DECIMALS;
-    address public RECIPIENT = makeAddr("recipient");
+    address public RECIPIENT = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address public RECIPIENT2 = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
@@ -68,13 +72,6 @@ contract GameTokenTest is Test {
         assertEq(gameToken.balanceOf(RECIPIENT),500 * 10 ** DECIMALS);
     }
 
-    function testBurn() public {
-        // Test burning tokens
-        vm.prank(RECIPIENT);
-        gameToken.burn(200 * 10 ** DECIMALS);
-        assertEq(gameToken.balanceOf(RECIPIENT), INITIAL_SUPPLY - 200 * 10 ** DECIMALS);
-    }
-
     function testTransferEmitsTraferEvent() public {
         // Test that transfer emits Transfer event
         vm.prank(OWNER);
@@ -106,20 +103,56 @@ contract GameTokenTest is Test {
         vm.prank(RECIPIENT);
         require(gameToken.approve(OWNER, 400 * 10 ** DECIMALS), "Approve failed");
 
-        // OWNER uses transferFrom to move tokens from RECIPIENT to themselves
+        // OWNER uses transferFrom to move tokens from RECIPIENT to RECIPIENT2
         vm.prank(OWNER);
-        require(gameToken.transferFrom(RECIPIENT, OWNER, 400 * 10 ** DECIMALS), "TransferFrom failed");
+        require(gameToken.transferFrom(RECIPIENT, RECIPIENT2, 400 * 10 ** DECIMALS), "TransferFrom failed");
         
-        assertEq(gameToken.balanceOf(OWNER), 400 * 10 ** DECIMALS);
+        assertEq(gameToken.balanceOf(RECIPIENT2), 400 * 10 ** DECIMALS);
         assertEq(gameToken.balanceOf(RECIPIENT), INITIAL_SUPPLY - 400 * 10 ** DECIMALS);
+        assertEq(gameToken.allowance(RECIPIENT, OWNER), 0);
     }
 
     function testCannotTransferFromWithoutApproval() public {
         // Test that transferFrom without approval fails
-        vm.startPrank(OWNER);
-        vm.expectRevert("ERC20: insufficient allowance");
+        vm.prank(OWNER);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientAllowance.selector, OWNER, 0, 100 * 10 ** DECIMALS
+            )
+        );
         gameToken.transferFrom(RECIPIENT, OWNER, 100 * 10 ** DECIMALS);
     }
 
+    function testBurn() public {
+        // Test burning tokens
+        uint256 initialBalance = gameToken.balanceOf(OWNER);
 
+        vm.prank(OWNER);
+        gameToken.burn(200 * 10 ** DECIMALS);
+        
+        assertEq(gameToken.totalSupply(), INITIAL_SUPPLY - 200 * 10 ** DECIMALS);    
+        assertEq(gameToken.balanceOf(OWNER), initialBalance - 200 * 10 ** DECIMALS);
+    }
+
+    function testBurnEmitsTransferEvent() public {
+        vm.startPrank(OWNER);
+        vm.expectEmit(true, true, false, true, address(diamondToken));
+        emit IERC20.Transfer(OWNER, address(0), 100 * 10 ** DECIMALS);
+        diamondToken.burn(100 * 10 ** DECIMALS);
+        vm.stopPrank();
+    }
+
+    function testCannotBurnWithoutApproval() public {
+        vm.prank(RECIPIENT);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                IERC20Errors.ERC20InsufficientAllowance.selector,
+                RECIPIENT,
+                0,
+                100 * 10 ** DECIMALS
+            )
+        );
+        diamondToken.burnFrom(OWNER, 100 * 10 ** DECIMALS);
+        vm.stopPrank();
+    }
 }
